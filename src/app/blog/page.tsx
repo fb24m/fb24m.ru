840230f26ee1923b9118ca6d97a454feb6507acf
ifb24m <fakem1ner@yandex.ru>
2023-12-01 15:46:15 +0400

    елка новогодняя

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,36 @@
+// 'use client';
+
+import styles from './index.module.scss';
 import React from 'react';
 
-import { Catalog } from '@/components/Catalog/Catalog.component'
+import { WordpressService } from '../../services/Wordpress';
+
+import { PostCard } from '@/components/PostCard';
 
 export default async function Blog() {
-	return <Catalog />
+	const { data: posts } = await WordpressService.getPosts();
+
+	console.log(posts);
+
+	if (!posts) return <>help@fb24m.ru</>
+	if (posts)
+		return (
+			<div className={`container container-grid ${styles.container}`}>
+				<div className="main">
+					{/* использование ссылки с ссылкой в компоненте */}
+					{posts ? posts.map((item) =>
+						<PostCard key={item.id}
+							title={item.title.rendered}
+							excerpt={item.excerpt.rendered}
+							image={''}
+							slug={item.slug}
+						/>
+					) : ''}
+				</div>
+				{/* <div className={styles.sidebar}>
+					<Sidebar /> 
+				</div> */}
+			</div>
+		);
+	else return <></>
 };
