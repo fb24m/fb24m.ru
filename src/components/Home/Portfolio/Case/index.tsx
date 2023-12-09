73479289d3e25efca28c6a4d6f59da4fe423001a
ifb24m <fakem1ner@yandex.ru>
2023-12-09 20:09:38 +0400

    optimized titles in cases

--- src/components/Home/Portfolio/Case/index.tsx
+++ src/components/Home/Portfolio/Case/index.tsx
@@ -2,7 +2,7 @@ import styles from './index.module.scss'
 import './index.scss'
 import type { HTMLAttributes, ReactElement } from 'react'
 
-import { Body1, Title2, Title4 } from '@/ui/components'
+import { Body1, Title2, Title3 } from '@/ui/components'
 
 import { WpImage } from '@/components/WpImage/WpImage.component'
 import { exists } from '@/functions/exists'
@@ -27,9 +27,9 @@ export const Case = async ({ title, imageId, label, review }: CaseProps): Promis
 				</div>
 
 				<div className={styles.info}>
-					<Title4 className="observe">{label}</Title4>
-					<Title2 className="observe">{title}</Title2>
-					<Body1 className="observe">Отзыв заказчика:</Body1>
+					<Title2 className={`${styles.subtitle} observe`}>{label}</Title2>
+					<Title3 className={`${styles.title} observe`}>{title}</Title3>
+					<Body1 className={`${styles.subtitle} observe`}>Отзыв заказчика:</Body1>
 					<WpImage className={styles.review} imageId={exists<number>(review)} />
 				</div>
 			</div>
